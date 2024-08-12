a40dc5613436f4ec0a67b3277594b9dba6f34dc0
Alexey Karaev <avkar.moscow@gmail.com>
2024-08-12 15:22:12 +0300

    fix input_data

--- insert_data.sql
+++ insert_data.sql
@@ -1,10 +1,4 @@
-INSERT INTO projects (nickname, full_name, real) VALUES
-('nick_1', 'person_1', 1),
-('nick_2', 'person_2', 1),
-('nick_4', 'bot_1', 0);
-
 INSERT INTO people (nickname, full_name, real) VALUES
 ('nick_1', 'person_1', 1),
 ('nick_2', 'person_2', 1),
 ('nick_4', 'bot_1', 0);
-
