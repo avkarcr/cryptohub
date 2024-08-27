2d61fc9b5b073ccd763ec64dabf10734b179a1a2
Alexey Karaev <avkar.moscow@gmail.com>
2024-08-27 23:50:25 +0300

    restructured DB - simplified

--- insert_initial_data.sql
+++ insert_initial_data.sql
@@ -1,3 +1,10 @@
+INSERT INTO activity_schemes (title, description) VALUES
+('full ownership', '100% to me'),
+('50/50', '50% to me, 50% to the other party'),
+('80/20', '80% to me, 20% to the other party'),
+('special', '90% to me, 10% to charity'),
+('donation', '100% to the other party');
+
 INSERT INTO activity_types (title, description) VALUES
 ('Testnet', 'Participation in testnets'),
 ('Ambassador', 'Ambassador program'),
@@ -17,8 +24,13 @@ INSERT INTO exchanges (title, description) VALUES
 
 INSERT INTO status (title) VALUES
 ('active'),
-('awaiting'),
-('ended');
+('distributing'),
+('closed');
+
+INSERT INTO wallet_states (state, description) VALUES
+('good', 'project has considered this wallet as eligible for reward or airdrop'),
+('idle', 'no rewards or airdrops without any Sybil mark'),
+('sybil', 'this wallet has been classified by the project as Sybil');
 
 INSERT INTO record_classification (class) VALUES
 ('full'),
